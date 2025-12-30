a211b434fd1e2dfc374494bf126cc8ac259d3ae7
lolItsHaiech <seyedamin13845@gmail.com>
2025-12-30 18:21:18 +0330

    add graham scan

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
+import math
+
 start = None
 goal = None
 obs = []
+
+
+def _srt_key(p, p1):
+    dy = p[1] - p1[1]
+    dx = p[0] - p1[0]
+    angle = math.atan2(dy, dx)
+    if angle < 0:
+        angle += 2 * math.pi
+    return angle
+
+
+def orientation(a, b, c):
+    return a[0]*(b[1] - c[1]) + b[0]*(c[1] - a[1]) + c[0]*(a[1] - b[1])  # simplified AB x AC
+
+
+def graham_scan(points: list[tuple]):
+    p1 = min(points, key=lambda p: p[1])
+    points.remove(p1)
+    srt = sorted(points, key=lambda p: _srt_key(p, p1), reverse=True)  # todo can be optimized using cross
+
+    stack = [p1, srt[0]]
+
+    for i in range(1, len(srt)):
+        while len(stack) >= 2 and orientation(stack[-2], stack[-1], srt[i]) >= 0:
+            stack.pop()
+        stack.append(srt[i])
+    return stack
+
 
 with open('input.txt', "r") as f:
     lines = [line.strip() for line in f if line.strip()]
@@ -26,8 +56,7 @@ while i < len(lines):
         num_points = int(lines[i + 1])
         polygon = []
         for j in range(num_points):
-            x, y = map(int, lines[i + 2 + j].split())
-            polygon.append([x, y])
+            polygon.append(tuple(map(int, lines[i + 2 + j].split())))
         obs.append(polygon)
         i += 2 + num_points
 
@@ -35,3 +64,4 @@ while i < len(lines):
         i += 1
 
 
+obs = [graham_scan(i) for i in obs]
