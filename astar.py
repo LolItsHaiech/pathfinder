f7d5c509369dfd7961593e30fb64bd5c0cf7c9e0
lolItsHaiech <seyedamin13845@gmail.com>
2026-02-13 22:05:54 +0330

    cleanup astar.py

--- astar.py
+++ astar.py
@@ -4,75 +4,71 @@ from p_queue import PQueue
 from tree import Tree
 
 
-class AStar:
-    def __init__(self, graph: dict[Node, list[tuple[Node, float]]], start: Node, goal: Node):
-        self.graph = graph
-        self.start = start
-        self.goal = goal
+def heuristic(node: Node, goal: Node) -> float:
+    dx = node.x - goal.x
+    dy = node.y - goal.y
+    return math.sqrt(dx ** 2 + dy ** 2)
 
-        self.tree = Tree()
-        self.frontier = PQueue()
-        self.visited: list[Node] = []
 
-    def heuristic(self, node: Node) -> float:
-        dx = node.x - self.goal.x
-        dy = node.y - self.goal.y
-        return math.sqrt(dx**2 + dy**2)
+def reconstruct_path(node: Node):
+    path = []
+    current = node
+    while current is not None:
+        path.append(current)
+        current = current.parent
+    path.reverse()
+    return path
 
-    def reconstruct_path(self, node: Node):
-        path = []
-        current = node
-        while current is not None:
-            path.append(current)
-            current = current.parent
-        path.reverse()
-        return path
 
-    def run(self):
-        # Zeroing all nodes
-        for node in self.graph.keys():
-            node.g = float('inf')
-            node.h = 0.0
-            node.f = float('inf')
-            node.parent = None
+def run(graph: dict[Node, list[tuple[Node, float]]], start: Node, goal: Node):
+    tree = Tree()
+    frontier = PQueue()
 
-        # initialize start node
-        self.start.g = 0.0
-        self.start.h = self.heuristic(self.start)
-        self.start.f = self.start.g + self.start.h
+    visited: list[Node] = []
+    # Zeroing all nodes
+    for node in graph.keys():
+        node.g = float('inf')
+        node.h = 0.0
+        node.f = float('inf')
+        node.parent = None
 
-        # add start node to tree
-        self.tree.insert(self.start)
+    # initialize start node
+    start.g = 0.0
+    start.h = heuristic(start, goal)
+    start.f = start.g + start.h
 
-        # add start node to frontier
-        self.frontier.enqueue(self.start, -self.start.f)
+    # add start node to tree
+    tree.insert(start)
 
-        while not self.frontier.is_empty():
-            current: Node = self.frontier.dequeue()
-            if current in self.visited:
+    # add start node to frontier
+    frontier.enqueue(start, -start.f)
+
+    while not frontier.is_empty():
+        current: Node = frontier.dequeue()
+        if current in visited:
+            continue
+        visited.append(current)
+        if current == goal:
+            # path found and returning the path
+            return reconstruct_path(current)
+
+        # expand neighbors
+        for neighbor, weight in graph[current]:
+            if neighbor in visited:
                 continue
-            self.visited.append(current)
-            if current == self.goal:
-                # path found and returning the path
-                return self.reconstruct_path(current)
+            new_g = current.g + weight
+            if new_g < neighbor.g:
+                neighbor.g = new_g
+                neighbor.h = heuristic(neighbor, goal)
+                neighbor.f = neighbor.g + neighbor.h
+                neighbor.parent = current
 
-            # expand neighbors
-            for neighbor, weight in self.graph[current]:
-                if neighbor in self.visited:
-                    continue
-                new_g = current.g + weight
-                if new_g < neighbor.g:
-                    neighbor.g = new_g
-                    neighbor.h = self.heuristic(neighbor)
-                    neighbor.f = neighbor.g + neighbor.h
-                    neighbor.parent = current
+                # add neighbor to frontier
+                frontier.enqueue(neighbor, -neighbor.f)
 
-                    # add neighbor to frontier
-                    self.frontier.enqueue(neighbor, -neighbor.f)
+                # add neighbor to tree
+                if tree.find(neighbor) is None:
+                    tree.insert(neighbor, parent_value=neighbor.parent)
 
-                    # add neighbor to tree
-                    if self.tree.find(neighbor) is None:
-                        self.tree.insert(neighbor, parent_value=neighbor.parent)
-
-        # if frontier is empty & goal is not reached, path not found
-        return None
+    # if frontier is empty & goal is not reached, path not found
+    return None
